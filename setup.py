ea08a890eaefdd1f6149c89b852216a4037731ec
Jkasnese <guilhermelw.uefs@gmail.com>
2020-09-17 06:45:48 -0300

    Add minj to pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Minimalist Journal on CLI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/guilhermelowa/minj",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
